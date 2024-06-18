c2aa6ded3e09bed442bc9c4bdca8d7e67de1fd98
Jayabrata18 <jayabrata180402@gmail.com>
2024-06-18 19:00:47 +0000

    restrudent started

--- apps/restaurants/src/main.ts
+++ apps/restaurants/src/main.ts
@@ -1,19 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { RestaurantsModule } from './restaurants.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
-  console.log('Dont Worry!!    🚀   users-microservice is running !!');
-
+  const appContext = await NestFactory.createApplicationContext(RestaurantsModule, { bufferLogs: true });
+  const configService = appContext.get(ConfigService);
+  const NATS_URI = configService.getOrThrow<string>('NATS_URI');
+  const logger = new Logger('Restaurants-Microservice');
+  logger.log('Starting Restaurants-Microservice bootstrap process...');
+  logger.log(`Connecting to NATS at ${NATS_URI}`);
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     RestaurantsModule,
+
     {
       transport: Transport.NATS,
       options: {
-        servers: ['nats://nats'],
+        servers: [NATS_URI],
       },
     },
   );
+
+  logger.log('Restaurants-Microservice created');
+  // app.setGlobalPrefix('Restaurantss');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // app.connectMicroservice<CustomStrategy>({
+  //   strategy: new NatsJetStreamServer({
+  //     connectionOptions: {
+  //       servers: configService.getOrThrow<string>('NATS_URI'),
+  //       name: 'Restaurants-listener',
+  //     },
+  //     consumerOptions: {
+  //       deliverGroup: 'Restaurants-group',
+  //       durable: 'Restaurants-durable',
+  //       deliverTo: 'Restaurants-messages',
+  //       manualAck: true,
+  //     },
+  //     streamConfig: {
+  //       name: 'RestaurantsStream',
+  //       subjects: ['Restaurants.>'],
+  //     },
+  //   }),
+  // });
   await app.listen();
+  logger.log('Restaurants-Microservice Bootstrap process completed');
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Restaurants-Microservice Bootstrap process failed with error:', error);
+});
--- apps/restaurants/src/restaurants.module.ts
+++ apps/restaurants/src/restaurants.module.ts
@@ -1,9 +1,53 @@
 import { Module } from '@nestjs/common';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
+import { NatsClientModule, pinoDevConfig, pinoProdConfig } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from 'nestjs-pino';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [NatsClientModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: true,  // Ignore unknown environment variables
+        abortEarly: false,   // Report all validation errors at once
+      },
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().required().error(new Error('NODE_ENV is required')),
+        NATS_URI: Joi.string().required().error(new Error('NATS_URI is required')),
+        POSTGRES_HOST: Joi.string().required().error(new Error('POSTGRES_HOST is required')),
+        POSTGRES_PORT: Joi.number().required().error(new Error('POSTGRES_PORT is required')),
+        POSTGRES_USER: Joi.string().required().error(new Error('POSTGRES_USER is required')),
+        POSTGRES_PASSWORD: Joi.string().required().error(new Error('POSTGRES_PASSWORD is required')),
+        POSTGRES_DB: Joi.string().required().error(new Error('POSTGRES_DB is required')),
+      }),
+    }),
+    LoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) =>
+        configService.getOrThrow<string>('NODE_ENV') === 'production'
+          ? pinoProdConfig()
+          : pinoDevConfig(),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [],
+        synchronize: true,  // Set to false in production
+      }),
+      inject: [ConfigService],
+    }),
+TypeOrmModule.forFeature([]),
+  ],
   controllers: [RestaurantsController],
   providers: [RestaurantsService],
 })
